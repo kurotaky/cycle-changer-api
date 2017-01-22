f256974dce416c5ca5c05988398e97dc3b475807
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2017-01-22 21:08:00 +0900

    fix config

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :cycle_changer, CycleChanger.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :cycle_changer, CycleChanger.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
